425eec4bb2a07d589c59a8afa684a5302629385e
longtao <chinaliulongtao@163.com>
2019-08-13 23:07:40 +0800

    fixed error

--- seco/seco/settings.py
+++ seco/seco/settings.py
@@ -81,8 +81,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'seco.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# Database https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
